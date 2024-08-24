7828f5c7c9a15b6a27f0bdd3760f820af762c571
karhy259 <karina.ospinabonilla@gmail.com>
2024-08-23 19:03:14 -0500

    entidad modulos

---
+++ src/app/modules/entidad/talento-humano/talento-humano.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-talento-humano',
+  standalone: true,
+  imports: [],
+  templateUrl: './talento-humano.component.html',
+  styleUrl: './talento-humano.component.css'
+})
+export class TalentoHumanoComponent {
+
+}
