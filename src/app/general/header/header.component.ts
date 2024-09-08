47265227cdfa11574b9247aedb4c3dd1a3f2009d
Youlez <karent.pinzon@idartes.gov.co>
2024-09-07 21:28:39 -0500

    arreglo del header

--- src/app/general/header/header.component.ts
+++ src/app/general/header/header.component.ts
@@ -1,15 +1,7 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  HostListener,
-  Input,
-  Renderer2,
-} from '@angular/core';
+import { Component, HostListener, Input, Renderer2 } from '@angular/core';
 import { MenuComponent } from './menu/menu.component';
 import { TopbarComponent } from './topbar/topbar.component';
 import { SearchComponent } from './search/search.component';
-import { debounceTime, fromEvent, throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -18,30 +10,10 @@ import { debounceTime, fromEvent, throttleTime } from 'rxjs';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent implements AfterViewInit {
-  @Input() minHeight?: number;
-  @Input('app-root') topOffset?: number;
+export class HeaderComponent {
+  @Input() tipo!: string;
 
-  private domElement: HTMLElement;
-
-  constructor(private renderer: Renderer2, private elementRef: ElementRef) {
-    this.domElement = this.elementRef.nativeElement as HTMLElement;
-    fromEvent(window, 'resize')
-      .pipe(throttleTime(1000), debounceTime(500))
-      .subscribe(() => this.setHeight());
-  }
-
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.setHeight();
-    }, 10);
-  }
-
-  private setHeight() {
-    let height = document.getElementById('header')?.offsetHeight;
-    this.renderer.setStyle(this.domElement, 'display', 'block');
-    this.renderer.setStyle(this.domElement, 'margin-top', `${height}px`);
-  }
+  constructor(private renderer: Renderer2) {}
 
   @HostListener('document:scroll', ['$event'])
   moveHeader(event: any) {
