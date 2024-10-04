30b595016772d5789f55b76547e0efed02abc130
JUAN <juan.torres@idartes.gov.co>
2024-10-04 11:10:28 -0500

    Ultimos Ajustes eventos

--- src/app/modules/sala-prensa/sala-prensa.module.ts
+++ src/app/modules/sala-prensa/sala-prensa.module.ts
@@ -4,11 +4,19 @@ import { NoticiasComponent } from './noticias/noticias.component';
 import arrayNoticias from './json/noticias.json';
 import { NoticiaInternaComponent } from './noticias/noticia-interna/noticia-interna.component';
 
+import { EventosComponent } from './eventos/eventos.component';
+
 let routes: Routes = [
   {
     path: 'noticias/57',
     title: 'Noticias',
     component: NoticiasComponent,
+  },
+
+  {
+    path: 'eventos/120',
+    title: 'Eventos',
+    component: EventosComponent,
   },
 ];
 
