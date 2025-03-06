71aa088439277701bc7ebb2af0d2813504a2b367
Youlez <karent.pinzon@idartes.gov.co>
2025-03-06 01:18:04 -0500

    cambios solicitados

--- src/app/modules/corporativo/asociaciones-ferrocarriles/asociaciones-ferrocarriles.component.ts
+++ src/app/modules/corporativo/asociaciones-ferrocarriles/asociaciones-ferrocarriles.component.ts
@@ -1,211 +1,276 @@
 import { Component } from '@angular/core';
 import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgFor, NgIf , CommonModule} from '@angular/common';
-import {asociacion} from '../../../interfaces/corporativo';
-import {AcordionFiles} from '../../../interfaces/acordion';
+import { NgFor, NgIf, CommonModule } from '@angular/common';
+import { asociacion } from '../../../interfaces/corporativo';
+import {
+  AcordionAnidadoFiles,
+  AcordionFiles,
+} from '../../../interfaces/acordion';
 
 @Component({
   selector: 'app-asociaciones-ferrocarriles',
   standalone: true,
   imports: [NgFor, NgbAccordionModule, NgIf, CommonModule],
   templateUrl: './asociaciones-ferrocarriles.component.html',
-  styleUrl: './asociaciones-ferrocarriles.component.css'
+  styleUrl: './asociaciones-ferrocarriles.component.css',
 })
-
-
 export class AsociacionesFerrocarrilesComponent {
+  infor: asociacion[] = [];
+  items: AcordionAnidadoFiles[] = [];
 
-  infor: asociacion [] = [];
-  items: AcordionFiles[] = [];
-
-  constructor (){
+  constructor() {
     this.infor = [
       {
-        id:1,
-        title: "Asociación de jubilados, pensionados y sustitutas del terminal maritimo de tumaco (AJUTERMARIT)",
-        direccion: "Calle Páez. Cra. 8 No. 78-22",
-        horario:"9:00AM A 2:30PM (Según citación)",
-        datos: "Carlos Jesus Angulo (Presidente AJUTERMARIT)",
-        telefono:"+57 311 488 98 19"
+        id: 1,
+        title:
+          'Asociación de jubilados, pensionados y sustitutas del terminal maritimo de tumaco (AJUTERMARIT)',
+        direccion: 'Calle Páez. Cra. 8 No. 78-22',
+        horario: '9:00AM A 2:30PM (Según citación)',
+        datos: 'Carlos Jesus Angulo (Presidente AJUTERMARIT)',
+        telefono: '+57 311 488 98 19',
       },
       {
-        id:2,
-        title: "Unión de pensionados portuarios de Santa Marta (LA UNIÒN)",
-        direccion: "Santa Marta Calle 17 No. 7 - 111 (Reuniones celebradas mensualmente)",
-        telefono:"+57 605 442 19 71",
-        correo:"launiondesantamarta@hotmail.com"
+        id: 2,
+        title: 'Unión de pensionados portuarios de Santa Marta (LA UNIÒN)',
+        direccion:
+          'Santa Marta Calle 17 No. 7 - 111 (Reuniones celebradas mensualmente)',
+        telefono: '+57 605 442 19 71',
+        correo: 'launiondesantamarta@hotmail.com',
       },
       {
-        id:3,
-        title: "Asociación de ferriviarios pensionados división Antioquia (AFEPENDA)",
-        direccion: "Carrera 51 No. 49-20 (Bello - Antioquia)",
-        telefono:"+57 599 18 82 - +57 311 723 43 04",
-        correo:"afependa_antioquia@yahoo.es / afependaasociacion@gmail.com"
+        id: 3,
+        title:
+          'Asociación de ferriviarios pensionados división Antioquia (AFEPENDA)',
+        direccion: 'Carrera 51 No. 49-20 (Bello - Antioquia)',
+        telefono: '+57 599 18 82 - +57 311 723 43 04',
+        correo: 'afependa_antioquia@yahoo.es, afependaasociacion@gmail.com',
       },
       {
-        id:4,
-        title: "Unión de pensionados ferroviarios UNIPENFERSANDER",
-        direccion: "Calle 35 No. 14-64 Oficina 203 Edificio Gómez Figueroa Barrio Centro Bucaramanga",
-        horario:"8:00AM a 12:00M de Lunes a Viernes",
-        telefono:"+57 305 310 56 96",
+        id: 4,
+        title: 'Unión de pensionados ferroviarios UNIPENFERSANDER',
+        direccion:
+          'Calle 35 No. 14-64 Oficina 203 Edificio Gómez Figueroa Barrio Centro Bucaramanga',
+        horario: '8:00AM a 12:00M de Lunes a Viernes',
+        telefono: '+57 305 310 56 96',
       },
       {
-        id:5,
-        title: "Asociación de pensionados de la desaparecida empresa Puertos de Colombia Luz del mundo",
-        direccion: "Carrera 36 No. 7 - 29 Barrio Eucarìstico (Templete) Santiago de Cali - Valle del Cauca",
-        telefono:"+57 315 790 03 47",
-        correo:"luzdelmundo2011@hotmail.com"
-      }
-    ]
+        id: 5,
+        title:
+          'Asociación de pensionados de la desaparecida empresa Puertos de Colombia Luz del mundo',
+        direccion:
+          'Carrera 36 No. 7 - 29 Barrio Eucarìstico (Templete) Santiago de Cali - Valle del Cauca',
+        telefono: '+57 315 790 03 47',
+        correo: 'luzdelmundo2011@hotmail.com',
+      },
+    ];
 
     this.items = [
       {
-        id:1,
-        nombre: "ASOC. DE FERROVIARIOS PENSIONADOS DIVISIÓN ANTIOQUIA (AFEPENDA)",
-        files:
-        [
-
+        id: 1,
+        nombre:
+          'ASOC. DE FERROVIARIOS PENSIONADOS DIVISIÓN ANTIOQUIA (AFEPENDA)',
+        files: [
           {
-            id:1,
-            tipo:"pdf",
-            url:"",
-            nombre:"ACTA ASAMBLEA ASOCIACIÓN DE FERROVIARIOS PENSIONADOS DIVISIÓN ANTIOQUIA AFEPENDA.pdf"
-
+            id: 1,
+            tipo: 'pdf',
+            url: '',
+            nombre:
+              'ACTA ASAMBLEA ASOCIACIÓN DE FERROVIARIOS PENSIONADOS DIVISIÓN ANTIOQUIA AFEPENDA.pdf',
           },
           {
-            id:2,
-            tipo:"pdf",
-            url:"",
-            nombre:"CERTIFICADO ASOCIACIÓN DE FERROVIARIOS PENSIONADOS DIVISIÓN ANTIOQUIA AFEPENDA.pdf"
-
+            id: 2,
+            tipo: 'pdf',
+            url: '',
+            nombre:
+              'CERTIFICADO ASOCIACIÓN DE FERROVIARIOS PENSIONADOS DIVISIÓN ANTIOQUIA AFEPENDA.pdf',
           },
           {
-            id:3,
-            tipo:"pdf",
-            url:"",
-            nombre:"ESTATUTOS APROBADOS ASOCIACIÓN DE FERROVIARIOS PENSIONADOS DIVISIÓN ANTIOQUIA AFEPENDA.pdf"
-
-          }
-        ]
+            id: 3,
+            tipo: 'pdf',
+            url: '',
+            nombre:
+              'ESTATUTOS APROBADOS ASOCIACIÓN DE FERROVIARIOS PENSIONADOS DIVISIÓN ANTIOQUIA AFEPENDA.pdf',
+          },
+        ],
       },
       {
-        id:1,
-        nombre: "ASOC. DE JUBILADOS, PENSIONADOS Y SUSTITUTAS DEL TERMINAL MARITIMO DE TUMACO (AJUTERMARIT)",
-        files:
-        [
-
+        id: 1,
+        nombre:
+          'ASOC. DE JUBILADOS, PENSIONADOS Y SUSTITUTAS DEL TERMINAL MARITIMO DE TUMACO (AJUTERMARIT)',
+        files: [
           {
-            id:1,
-            tipo:"pdf",
-            url:"",
-            nombre:"ASAMBLEA GENERAL JUNTA DIRECTIVA ASOCIACIÓN DE JUBILADOS, PENSIONADOS Y SUSTITUTAS DEL TERMINAL MARITIMO DE TUMACO AJUTERMARIT.pdf"
-
+            id: 1,
+            tipo: 'pdf',
+            url: '',
+            nombre:
+              'ASAMBLEA GENERAL JUNTA DIRECTIVA ASOCIACIÓN DE JUBILADOS, PENSIONADOS Y SUSTITUTAS DEL TERMINAL MARITIMO DE TUMACO AJUTERMARIT.pdf',
           },
           {
-            id:2,
-            tipo:"pdf",
-            url:"",
-            nombre:"INFORMACIÓN GENERAL ASOCIACIÓN DE JUBILADOS, PENSIONADOS Y SUSTITUTAS DEL TERMINAL MARITIMO DE TUMACO AJUTERMARIT.pdf"
-
+            id: 2,
+            tipo: 'pdf',
+            url: '',
+            nombre:
+              'INFORMACIÓN GENERAL ASOCIACIÓN DE JUBILADOS, PENSIONADOS Y SUSTITUTAS DEL TERMINAL MARITIMO DE TUMACO AJUTERMARIT.pdf',
           },
           {
-            id:3,
-            tipo:"pdf",
-            url:"",
-            nombre:"RESOLUCIÓN MIN. TRABAJO POR MEDIO DE LA CUAL ORDENA INSCRIPCIÓN ASOCIACIÓN DE JUBILADOS, PENSIONADOS Y SUSTITUTAS DEL TERMINAL MARITIMO DE TUMACO AJUTERMARIT.pdf"
-
-          }
-        ]
+            id: 3,
+            tipo: 'pdf',
+            url: '',
+            nombre:
+              'RESOLUCIÓN MIN. TRABAJO POR MEDIO DE LA CUAL ORDENA INSCRIPCIÓN ASOCIACIÓN DE JUBILADOS, PENSIONADOS Y SUSTITUTAS DEL TERMINAL MARITIMO DE TUMACO AJUTERMARIT.pdf',
+          },
+        ],
       },
       {
-        id:1,
-        nombre: "ASOC. DE PENSIONADOS DE LA DESAPARECIDA EMPRESA PUERTOS DE COLOMBIA LUZ DEL MUNDO",
-        files:
-        [
-
+        id: 1,
+        nombre:
+          'ASOC. DE PENSIONADOS DE LA DESAPARECIDA EMPRESA PUERTOS DE COLOMBIA LUZ DEL MUNDO',
+        files: [
           {
-            id:1,
-            tipo:"pdf",
-            url:"/assets/files/corporativo/asociaciones_ferrocarriles/ASOC.%20DE%20PENSIONADOS%20DE%20LA%20DESAPARECIDA%20EMPRESA/ACTA%20NOMBRAMIENTO%20ASOCIACION%20DE%20PENSIONADOS%20DE%20LA%20DESAPARECIDA%20EMPRESA%20PUERTOS%20DE%20COLOMBIA%20LUZ%20D.pdf",
-            nombre:"ACTA NOMBRAMIENTO ASOCIACION DE PENSIONADOS DE LA DESAPARECIDA EMPRESA PUERTOS DE COLOMBIA LUZ DEL MUNDO.pdf"
-
+            id: 1,
+            tipo: 'pdf',
+            url: '/assets/files/corporativo/asociaciones_ferrocarriles/ASOC.%20DE%20PENSIONADOS%20DE%20LA%20DESAPARECIDA%20EMPRESA/ACTA%20NOMBRAMIENTO%20ASOCIACION%20DE%20PENSIONADOS%20DE%20LA%20DESAPARECIDA%20EMPRESA%20PUERTOS%20DE%20COLOMBIA%20LUZ%20D.pdf',
+            nombre:
+              'ACTA NOMBRAMIENTO ASOCIACION DE PENSIONADOS DE LA DESAPARECIDA EMPRESA PUERTOS DE COLOMBIA LUZ DEL MUNDO.pdf',
           },
           {
-            id:2,
-            tipo:"pdf",
-            url:"/assets/files/corporativo/asociaciones_ferrocarriles/ASOC.%20DE%20PENSIONADOS%20DE%20LA%20DESAPARECIDA%20EMPRESA/ASOCIACION%20DE%20PENSIONADOS%20DE%20LA%20DESAPARECIDA%20EMPRESA%20PUERTOS%20DE%20COLOMBIA%20LUZ%20DEL%20MUNDO.pdf",
-            nombre:"ASOCIACION DE PENSIONADOS DE LA DESAPARECIDA EMPRESA PUERTOS DE COLOMBIA LUZ DEL MUNDO.pdf"
-
+            id: 2,
+            tipo: 'pdf',
+            url: '/assets/files/corporativo/asociaciones_ferrocarriles/ASOC.%20DE%20PENSIONADOS%20DE%20LA%20DESAPARECIDA%20EMPRESA/ASOCIACION%20DE%20PENSIONADOS%20DE%20LA%20DESAPARECIDA%20EMPRESA%20PUERTOS%20DE%20COLOMBIA%20LUZ%20DEL%20MUNDO.pdf',
+            nombre:
+              'ASOCIACION DE PENSIONADOS DE LA DESAPARECIDA EMPRESA PUERTOS DE COLOMBIA LUZ DEL MUNDO.pdf',
           },
           {
-            id:3,
-            tipo:"pdf",
-            url:"/assets/files/corporativo/asociaciones_ferrocarriles/ASOC.%20DE%20PENSIONADOS%20DE%20LA%20DESAPARECIDA%20EMPRESA/NOMINA%20DE%20SOCIOS%20ASOCIACION%20DE%20PENSIONADOS%20DE%20LA%20DESAPARECIDA%20EMPRESA%20PUERTOS%20DE%20COLOMBIA%20LUZ%20DEL%20MUNDO.pdf",
-            nombre:"NOMINA DE SOCIOS ASOCIACION DE PENSIONADOS DE LA DESAPARECIDA EMPRESA PUERTOS DE COLOMBIA LUZ DEL MUNDO.pdf"
-
+            id: 3,
+            tipo: 'pdf',
+            url: '/assets/files/corporativo/asociaciones_ferrocarriles/ASOC.%20DE%20PENSIONADOS%20DE%20LA%20DESAPARECIDA%20EMPRESA/NOMINA%20DE%20SOCIOS%20ASOCIACION%20DE%20PENSIONADOS%20DE%20LA%20DESAPARECIDA%20EMPRESA%20PUERTOS%20DE%20COLOMBIA%20LUZ%20DEL%20MUNDO.pdf',
+            nombre:
+              'NOMINA DE SOCIOS ASOCIACION DE PENSIONADOS DE LA DESAPARECIDA EMPRESA PUERTOS DE COLOMBIA LUZ DEL MUNDO.pdf',
           },
           {
-            id:4,
-            tipo:"pdf",
-            url:"/assets/files/corporativo/asociaciones_ferrocarriles/ASOC.%20DE%20PENSIONADOS%20DE%20LA%20DESAPARECIDA%20EMPRESA/REP.%20LEGAL%20ASOCIACION%20DE%20PENSIONADOS%20DE%20LA%20DESAPARECIDA%20EMPRESA%20PUERTOS%20DE%20COLOMBIA%20LUZ%20DEL%20MUND.pdf",
-            nombre:"REP. LEGAL ASOCIACION DE PENSIONADOS DE LA DESAPARECIDA EMPRESA PUERTOS DE COLOMBIA LUZ DEL MUNDO.pdf"
-
-          }
-        ]
+            id: 4,
+            tipo: 'pdf',
+            url: '/assets/files/corporativo/asociaciones_ferrocarriles/ASOC.%20DE%20PENSIONADOS%20DE%20LA%20DESAPARECIDA%20EMPRESA/REP.%20LEGAL%20ASOCIACION%20DE%20PENSIONADOS%20DE%20LA%20DESAPARECIDA%20EMPRESA%20PUERTOS%20DE%20COLOMBIA%20LUZ%20DEL%20MUND.pdf',
+            nombre:
+              'REP. LEGAL ASOCIACION DE PENSIONADOS DE LA DESAPARECIDA EMPRESA PUERTOS DE COLOMBIA LUZ DEL MUNDO.pdf',
+          },
+        ],
       },
       {
-        id:1,
-        nombre: "UNIÓN DE PENSIONADOS FERROVIARIOS UNIPENFERSANDER",
-        files:
-        [
-
+        id: 1,
+        nombre:
+          'ASOCIACIÓN DE JUBILADOS Y PENSIONADOS DE EMPRESA PUERTOS DE COLOMBIA',
+        files: [
           {
-            id:1,
-            tipo:"pdf",
-            url:"/assets/files/corporativo/asociaciones_ferrocarriles/UNI%c3%93N%20DE%20PENSIONADOS%20FERROVIARIOS%20UNIPENFERSANDER/INFORMACI%c3%93N%20GENERAL%20UNI%c3%93N%20DE%20PENSIONADOS%20FERROVIARIOS%20UNIPENFERSANDER.pdf",
-            nombre:"INFORMACIÓN GENERAL UNIÓN DE PENSIONADOS FERROVIARIOS UNIPENFERSANDER.pdf"
-
-          }
-        ]
+            id: 1,
+            tipo: 'pdf',
+            url: '/assets/files/corporativo/asociaciones_ferrocarriles/ASOCIACION DE JUBILADOS Y PENSIONADOS DE EMPRESA PUERTOS DE COLOMBIA/JUNTA DIRECTIVA  AJUPECOL.pdf',
+            nombre: 'JUNTA DIRECTIVA  AJUPECOL.pdf',
+          },
+        ],
       },
       {
-        id:1,
-        nombre: "UNIÓN DE PENSIONADOS PORTUARIOS DE SANTA MARTA (LA UNIÒN)",
-        files:
-        [
-
+        id: 1,
+        nombre: 'CÓMITES LOCALES',
+        acordion: [
           {
-            id:1,
-            tipo:"pdf",
-            url:"",
-            nombre:"ACTA REEMPLAZO POR FALLECIMIENTO UNIÓN DE PENSIONADOS PORTUARIOS DE SANTA MARTA LA UNIÒN.pdf"
-
+            id: 1,
+            nombre: '2025',
+            files: [
+              {
+                id: 1,
+                tipo: 'pdf',
+                url: '/assets/files/corporativo/asociaciones_ferrocarriles/COMITES LOCALES/2025/COMITES LOCALES.pdf',
+                nombre: 'COMITES LOCALES.pdf',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        id: 1,
+        nombre: 'DIRECTORIO DE ASOCIACIONES DE PENSIONADOS DE FERROCARRILES',
+        files: [
+          {
+            id: 1,
+            tipo: 'pdf',
+            url: '/assets/files/corporativo/asociaciones_ferrocarriles/DIRECTORIO DE ASOCIACIONES DE PENSIONADOS DE FERROCARRILES/DIRECTORIO ASOCIACIONES DE USUARIOS FCN.pdf',
+            nombre: 'DIRECTORIO ASOCIACIONES DE USUARIOS FCN.pdf',
           },
           {
-            id:2,
-            tipo:"pdf",
-            url:"",
-            nombre:"ESTATUTOS UNIÓN DE PENSIONADOS PORTUARIOS DE SANTA MARTA LA UNIÒN.pdf"
-
+            id: 2,
+            tipo: 'pdf',
+            url: '/assets/files/corporativo/asociaciones_ferrocarriles/DIRECTORIO DE ASOCIACIONES DE PENSIONADOS DE FERROCARRILES/DIRECTORIO ASOCIACIONES DE USUARIOS PNC.pdf',
+            nombre: 'DIRECTORIO ASOCIACIONES DE USUARIOS PNC.pdf',
+          },
+        ],
+      },
+      {
+        id: 1,
+        nombre:
+          'ONG VEEDURIA CIUDADANA NACIONAL PARA LA DEFENSA DE LOS PENSIONADOS',
+        files: [
+          {
+            id: 1,
+            tipo: 'pdf',
+            url: '/assets/files/corporativo/asociaciones_ferrocarriles/ONG VEEDURIA CIUDADANA NACIONAL PARA LA DEFENSA DE LOS PENSIONADOS/OFICIO VEEDORES BARRANQUILLA.pdf',
+            nombre: 'OFICIO VEEDORES BARRANQUILLA.pdf',
           },
           {
-            id:3,
-            tipo:"pdf",
-            url:"",
-            nombre:"INFORMACIÓN GENERAL UNIÓN DE PENSIONADOS PORTUARIOS DE SANTA MARTA LA UNIÒN.pdf"
-
+            id: 1,
+            tipo: 'pdf',
+            url: '/assets/files/corporativo/asociaciones_ferrocarriles/ONG VEEDURIA CIUDADANA NACIONAL PARA LA DEFENSA DE LOS PENSIONADOS/VEEDORES BARANQUILLA.pdf',
+            nombre: 'VEEDORES BARANQUILLA.pdf',
+          },
+        ],
+      },
+      {
+        id: 1,
+        nombre: 'UNIÓN DE PENSIONADOS FERROVIARIOS UNIPENFERSANDER',
+        files: [
+          {
+            id: 1,
+            tipo: 'pdf',
+            url: '/assets/files/corporativo/asociaciones_ferrocarriles/UNI%c3%93N%20DE%20PENSIONADOS%20FERROVIARIOS%20UNIPENFERSANDER/INFORMACI%c3%93N%20GENERAL%20UNI%c3%93N%20DE%20PENSIONADOS%20FERROVIARIOS%20UNIPENFERSANDER.pdf',
+            nombre:
+              'INFORMACIÓN GENERAL UNIÓN DE PENSIONADOS FERROVIARIOS UNIPENFERSANDER.pdf',
+          },
+        ],
+      },
+      {
+        id: 1,
+        nombre: 'UNIÓN DE PENSIONADOS PORTUARIOS DE SANTA MARTA (LA UNIÒN)',
+        files: [
+          {
+            id: 1,
+            tipo: 'pdf',
+            url: '',
+            nombre:
+              'ACTA REEMPLAZO POR FALLECIMIENTO UNIÓN DE PENSIONADOS PORTUARIOS DE SANTA MARTA LA UNIÒN.pdf',
           },
           {
-            id:4,
-            tipo:"pdf",
-            url:"",
-            nombre:"PRESIDENTE JUNTA DIRECTIVA UNIÓN DE PENSIONADOS PORTUARIOS DE SANTA MARTA LA UNIÒN.pdf"
-
-          }
-        ]
-      }
-
-
-    ]
+            id: 2,
+            tipo: 'pdf',
+            url: '',
+            nombre:
+              'ESTATUTOS UNIÓN DE PENSIONADOS PORTUARIOS DE SANTA MARTA LA UNIÒN.pdf',
+          },
+          {
+            id: 3,
+            tipo: 'pdf',
+            url: '',
+            nombre:
+              'INFORMACIÓN GENERAL UNIÓN DE PENSIONADOS PORTUARIOS DE SANTA MARTA LA UNIÒN.pdf',
+          },
+          {
+            id: 4,
+            tipo: 'pdf',
+            url: '',
+            nombre:
+              'PRESIDENTE JUNTA DIRECTIVA UNIÓN DE PENSIONADOS PORTUARIOS DE SANTA MARTA LA UNIÒN.pdf',
+          },
+        ],
+      },
+    ];
   }
 }
--- src/app/modules/participa/consulta-ciudadana/consulta-ciudadana.component.ts
+++ src/app/modules/participa/consulta-ciudadana/consulta-ciudadana.component.ts
@@ -1,112 +1,106 @@
-import { Component } from '@angular/core'; 
+import { Component } from '@angular/core';
 import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 import { AcordionFiles } from '../../../interfaces/acordion';
-import { NgFor } from '@angular/common'; 
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-consulta-ciudadana',
   standalone: true,
   imports: [NgbAccordionModule, NgFor],
   templateUrl: './consulta-ciudadana.component.html',
-  styleUrl: './consulta-ciudadana.component.css'
+  styleUrl: './consulta-ciudadana.component.css',
 })
 export class ConsultaCiudadanaComponent {
-
   items: AcordionFiles[] = [];
   constructor() {
     this.items = [
-        {
-          id: 1,
-        nombre: "Tema de consulta (normas, políticas, programas o proyectos) y resumen del mismo",
+      {
+        id: 1,
+        nombre:
+          'Tema de consulta (normas, políticas, programas o proyectos) y resumen del mismo',
         files: [
-          
-            {
-              id: 1,
-              tipo: "pdf",
-              url: "assets/files/contenidos-sobre-consulta-ciudadana/01 Tema de consulta/01. 2022/ANEXO 3 - FIRMAS ACTA DE SENSIBILIZACION.pdf",
-              nombre: "ANEXO 3 - FIRMAS ACTA DE SENSIBILIZACIÓN.pdf"
-            },
-            {
-              id: 1,
-              tipo: "pdf",
-              url: "assets/files/contenidos-sobre-consulta-ciudadana/01 Tema de consulta/01. 2022/MODELO DE ATENCION INTEGRAL EN SALUD PARA FERROCARRILES NACIONALES (MAISFEN).pdf",
-              nombre: "MODELO DE ATENCIÓN INTEGRAL EN SALUD PARA FERROCARRILES NACIONALES (MAISFEN).pdf"
-            }
-          ],
-        },
-        {
-          id: 2,
-        nombre: "Habilitar canales de consulta y caja de herramientas",
-        files: [
-            {
-              id: 1,
-              tipo: "",
-              url: "",
-              nombre: ""
-            }
-          ],
-        },
           {
-          id: 3,
-        nombre: "Publicar observaciones y comentarios y las respuestas de proyectos normativos",
+            id: 1,
+            tipo: 'pdf',
+            url: 'assets/files/contenidos-sobre-consulta-ciudadana/01 Tema de consulta/01. 2022/ANEXO 3 - FIRMAS ACTA DE SENSIBILIZACION.pdf',
+            nombre: 'ANEXO 3 - FIRMAS ACTA DE SENSIBILIZACIÓN.pdf',
+          },
+          {
+            id: 1,
+            tipo: 'pdf',
+            url: 'assets/files/contenidos-sobre-consulta-ciudadana/01 Tema de consulta/01. 2022/MODELO DE ATENCION INTEGRAL EN SALUD PARA FERROCARRILES NACIONALES (MAISFEN).pdf',
+            nombre:
+              'MODELO DE ATENCIÓN INTEGRAL EN SALUD PARA FERROCARRILES NACIONALES (MAISFEN).pdf',
+          },
+        ],
+      },
+      {
+        id: 2,
+        nombre: 'Habilitar canales de consulta y caja de herramientas',
         files: [
-            {
-              id: 1,
-              tipo: "",
-              url: "",
-              nombre: ""
-            }
-          ],
+          {
+            id: 1,
+            tipo: '',
+            url: '',
+            nombre: '',
+          },
+        ],
+      },
+      {
+        id: 3,
+        nombre:
+          'Publicar observaciones y comentarios y las respuestas de proyectos normativos',
+        files: [
+          {
+            id: 1,
+            tipo: '',
+            url: '',
+            nombre: '',
+          },
+        ],
       },
       {
         id: 4,
-      nombre: "Crear un enlace que redireccione a la Sección Normativa",
-      files: [
+        nombre: 'Crear un enlace que redireccione a la Sección Normativa',
+        files: [
           {
             id: 1,
-            tipo: "",
-            url: "",
-            nombre: ""
-          }
+            tipo: '',
+            url: '',
+            nombre: '',
+          },
         ],
       },
       {
         id: 5,
-      nombre: "Facilitar herramienta de evaluación",
-      files: [
+        nombre: 'Facilitar herramienta de evaluación',
+        files: [
           {
             id: 1,
-            tipo: "",
-            url: "",
-            nombre: ""
-          }
+            tipo: '',
+            url: '',
+            nombre: '',
+          },
         ],
       },
       {
         id: 6,
-      nombre: "Planeación estratégica y Planes Institucionales",
-      files: [
+        nombre: 'Planeación estratégica y Planes Institucionales',
+        files: [
           {
             id: 1,
-            tipo: "excel",
-            url: "assets/files/contenidos-sobre-consulta-ciudadana/06 Planeación estratégica y Planes Institucionales/Plan Anticorrupción y Atención al Ciudadano - Preeliminar.xlsx",
-            nombre: "Plan Anticorrupción y Atención al Ciudadano - Preeliminar.xlsx"
+            tipo: 'excel',
+            url: 'assets/files/contenidos-sobre-consulta-ciudadana/06 Planeación estratégica y Planes Institucionales/PROYECTO PLAN DE ACCIÓN 2025.xlsx',
+            nombre: 'PROYECTO PLAN DE ACCIÓN 2025.xlsx',
           },
           {
             id: 2,
-            tipo: "excel",
-            url: "assets/files/contenidos-sobre-consulta-ciudadana/06 Planeación estratégica y Planes Institucionales/Plan de Acción 2024 – Preliminar.xlsx",
-            nombre: "Plan de Acción 2024 – Preliminar.xlsx"
+            tipo: 'excel',
+            url: 'assets/files/contenidos-sobre-consulta-ciudadana/06 Planeación estratégica y Planes Institucionales/PROYECTO PLAN ESTRATÉGICO 2025.xlsx',
+            nombre: 'PROYECTO PLAN ESTRATÉGICO 2025.xlsx',
           },
-          {
-            id: 3,
-            tipo: "excel",
-            url: "assets/files/contenidos-sobre-consulta-ciudadana/06 Planeación estratégica y Planes Institucionales/Planes Estratégico Institucional 2024–2026 – Preliminar.xlsx",
-            nombre: "Planes Estratégico Institucional 2024–2026 – Preliminar.xlsx"
-          }
         ],
       },
     ];
   }
-
 }
