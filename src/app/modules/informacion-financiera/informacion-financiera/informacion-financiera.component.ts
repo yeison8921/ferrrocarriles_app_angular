2d47516382bd208df9cc71e365c88557dabb7493
Wilcabo <wilcabo2011@hotmail.com>
2025-03-17 12:06:01 -0500

    CAmbio

--- src/app/modules/informacion-financiera/informacion-financiera/informacion-financiera.component.ts
+++ src/app/modules/informacion-financiera/informacion-financiera/informacion-financiera.component.ts
@@ -75,8 +75,8 @@ export class InformacionFinancieraComponent {
               {
                 id: 8,
                 tipo: 'pdf',
-                nombre: 'REP_ING031_InformeEjecucionINGRESOS VIGENCIA DECRETO LIQUIDACION - 2025-03-04 A FEBRERO 28 2025-.pdf',
-                url: 'assets/files/presupuesto/02.%202025/02%20FEBRERO/REP_ING031_InformeEjecucionINGRESOSVIGENCIADECRETOLIQUIDACION-2025-03-04AFEBRERO282025-.pdf',
+                nombre: 'REP_ING031_Informe Ejecucion INGRESOS VIGENCIA DECRETO LIQUIDACION - 2025-03-04 A FEBRERO 28 2025-.pdf',
+                url: 'assets/files/presupuesto/11.2025/02.FEBRERO/REP_ING031_InformeEjecucionINGRESOSVIGENCIADECRETOLIQUIDACION-2025-03-04AFEBRERO282025-.pdf',
               },
             ],
           },
