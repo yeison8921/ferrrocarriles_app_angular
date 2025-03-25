201fc825f3fb2cf2175460329267875af9a3cdba
Wilcabo <wilcabo2011@hotmail.com>
2025-03-25 17:57:52 -0500

    cambio pqrs informe documento

--- src/app/modules/informes/informes-pqrs/informes-pqrs.component.ts
+++ src/app/modules/informes/informes-pqrs/informes-pqrs.component.ts
@@ -30,6 +30,20 @@ export class InformesPqrsComponent {
             nombre: 'INFORME PQRSDF II TRIMESTRE 2024.pdf',
             url: '/assets/files/pqrs/01. 2024/INFORME PQRSDF II TRIMESTRE 2024.pdf',
           },
+
+          {
+            id: 3,
+            tipo: 'pdf',
+            nombre: 'INFORME PQRSD III TRIMESTRE 2024.pdf',
+            url: '/assets/files/pqrs/01. 2024/INFORME PQRSD III TRIMESTRE 2024.pdf',
+          },
+
+          {
+            id: 4,
+            tipo: 'pdf',
+            nombre: 'INFORME PQRSDF IV TRIMESTRE 2024.pdf',
+            url: '/assets/files/pqrs/01. 2024/INFORME PQRSDF IV TRIMESTRE 2024.pdf',
+          },
         ],
       },
       {
