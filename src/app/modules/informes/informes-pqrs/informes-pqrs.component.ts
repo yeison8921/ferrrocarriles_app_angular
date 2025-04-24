14c1282fb36b12f9a78b450db514857dee3e5a44
Wilcabo <wilcabo2011@hotmail.com>
2025-04-24 12:31:54 -0500

    tERCER TRIMESTRE PQRSD

--- src/app/modules/informes/informes-pqrs/informes-pqrs.component.ts
+++ src/app/modules/informes/informes-pqrs/informes-pqrs.component.ts
@@ -30,8 +30,15 @@ export class InformesPqrsComponent {
             nombre: 'INFORME PQRSDF II TRIMESTRE 2024.pdf',
             url: '/assets/files/pqrs/01. 2024/INFORME PQRSDF II TRIMESTRE 2024.pdf',
           },
+
           {
             id: 3,
+            tipo: 'pdf',
+            nombre: 'INFORME PQRSDF III TRIMESTRE 2024.pdf',
+            url: '/assets/files/pqrs/01. 2024/INFORME PQRSD III TRIMESTRE 2024.pdf',
+          },
+          {
+            id: 4,
             tipo: 'pdf',
             nombre: 'INFORME PQRSDF IV TRIMESTRE 2024.pdf',
             url: '/assets/files/pqrs/01. 2024/INFORME PQRSDF IV TRIMESTRE 2024 mm.pdf',
