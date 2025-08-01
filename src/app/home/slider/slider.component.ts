ab4f3ece1c58dadefcfa7236050682cc508c1761
Wilcabo <wilcabo2011@hotmail.com>
2025-08-01 16:13:46 -0500

    slider

--- src/app/home/slider/slider.component.ts
+++ src/app/home/slider/slider.component.ts
@@ -28,7 +28,11 @@ export class SliderComponent {
   constructor() {
     this.slider = [
      
-     
+       {
+        id: 6,
+        imagen: '/assets/img/Home/sliderHome/RADICACION MODULOFACTURACIONDEURGENCIAS.png',
+        url: 'https://fps.horus-health.com/',
+      },
       {
         id: 7,
         imagen: '/assets/img/Home/sliderHome/4139.png',
