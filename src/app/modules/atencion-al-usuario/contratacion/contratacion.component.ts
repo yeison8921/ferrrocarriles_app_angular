168fe10dce8633e48fc05264f29ff423f50a65bd
Wilcabo <wilcabo2011@hotmail.com>
2025-10-16 14:04:36 -0500

    CAMBIO

--- src/app/modules/atencion-al-usuario/contratacion/contratacion.component.ts
+++ src/app/modules/atencion-al-usuario/contratacion/contratacion.component.ts
@@ -137,6 +137,12 @@ export class ContratacionComponent {
                 nombre: 'CONTRATOS SUSCRITOS EN AGOSTO 2025',
                 url: 'assets/files/contract/CONTRATACIÓN DIRECTA/01. 2025/CONTRATACION DIRECTA AGOSTO DE 2025.xlsx',
               },
+               {
+                id: 6,
+                tipo: 'excel',
+                nombre: 'CONTRATOS SUSCRITOS EN SEPTIEMBRE 2025',
+                url: 'assets/files/contract/CONTRATACIÓN DIRECTA/01. 2025/Contratacion Celebrada Septiembre de 2025.xlsx',
+              },
 
             ],
           },
