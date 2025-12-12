1036aac1c61c75f4e58eaa7813567d3e1215b392
Wilcabo <wilcabo2011@hotmail.com>
2025-12-12 11:07:22 -0500

    cambio

--- src/app/modules/tramites-servicios/certificadodepension/certificadodepension.ts
+++ src/app/modules/tramites-servicios/certificadodepension/certificadodepension.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-certificadodepension',
+  standalone: true,
   imports: [],
   templateUrl: './certificadodepension.html',
   styleUrl: './certificadodepension.css'
