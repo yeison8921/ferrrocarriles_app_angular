d924f6d183e5b547d3b00811240ae4cb9939fbd4
Wilcabo <wilcabo2011@hotmail.com>
2025-12-12 13:31:15 -0500

    yy

--- src/app/modules/tramites-servicios/certificadodepension/certificadodepension.ts
+++ src/app/modules/tramites-servicios/certificadodepension/certificadodepension.ts
@@ -29,7 +29,7 @@ export class Certificadodepension {
     this.resultado = null;
     this.pdfURL = null;
 
-    this.http.post('https://edq.com.co/certificado.php', { documento: this.documento })
+    this.http.post('http://192.168.100.4/fps-backend/certificado.php', { documento: this.documento })
       .subscribe({
         next: (resp: any) => {
           this.cargando = false;
