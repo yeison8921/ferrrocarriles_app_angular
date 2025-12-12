e1e77609f5606abac5dd63bc37b38cf6e56bcc36
Wilcabo <wilcabo2011@hotmail.com>
2025-12-12 12:18:25 -0500

    tt

--- src/app/modules/tramites-servicios/certificadodepension/certificadodepension.ts
+++ src/app/modules/tramites-servicios/certificadodepension/certificadodepension.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { switchMap } from 'rxjs/operators';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-certificadodepension',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './certificadodepension.html',
   styleUrls: ['./certificadodepension.css']
 })
@@ -18,14 +17,6 @@ export class Certificadodepension {
   cargando = false;
   error = '';
 
-  // Usuario y clave quemados
-  private usuario = 'safixFPasivoTest';
-  private clave = 'C0n3ctfP4S1V0T3st';
-
-  // Endpoints
-  private urlLogin = 'https://apix.fps.gov.co/Certificados/api/login/authenticate';
-  private urlCertificado = 'https://apix.fps.gov.co/Certificados/api/Certificados/ConsultaCertificadoPensionados';
-
   constructor(private http: HttpClient) {}
 
   buscar() {
@@ -35,32 +26,19 @@ export class Certificadodepension {
     this.error = '';
     this.resultado = null;
 
-    // 1️⃣ Login para obtener token
-    const bodyLogin = { username: this.usuario, password: this.clave };
-
-    this.http.post<{ token: string }>(this.urlLogin, bodyLogin)
-      .pipe(
-        // 2️⃣ Usar token para consultar certificado
-        switchMap((resp: { token: any; }) => {
-          const headers = new HttpHeaders({
-            'Authorization': `Bearer ${resp.token}`
-          });
-
-          const url = `${this.urlCertificado}?Identificacion=${this.documento}&AnoPago=2025`;
-
-          return this.http.get(url, { headers });
-        })
-      )
+    this.http.post('http://192.168.100.4/fps-backend/certificado.php', { documento: this.documento })
       .subscribe({
         next: (resp: any) => {
           this.resultado = resp;
           this.cargando = false;
         },
-        error: () => {
+        error: (err: any) => {
           this.error = 'No se pudo consultar el certificado.';
           this.cargando = false;
+          console.error(err);
         }
       });
   }
 }
+
 
