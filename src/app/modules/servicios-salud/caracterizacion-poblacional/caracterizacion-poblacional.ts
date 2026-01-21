c6d3cd3940df71027337e2c9f6460a3cfdf0d059
Wilcabo <wilcabo2011@hotmail.com>
2026-01-21 12:30:31 -0500

    Caracterizacion

---
+++ src/app/modules/servicios-salud/caracterizacion-poblacional/caracterizacion-poblacional.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-caracterizacion-poblacional',
+  imports: [],
+  templateUrl: './caracterizacion-poblacional.html',
+  styleUrl: './caracterizacion-poblacional.css',
+})
+export class CaracterizacionPoblacional {
+
+}
