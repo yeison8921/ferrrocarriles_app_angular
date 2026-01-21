ffc81f6172f8782a4278f7b0056606e89c81bff4
Wilcabo <wilcabo2011@hotmail.com>
2026-01-21 12:57:14 -0500

    ok

--- src/app/modules/servicios-salud/caracterizacion-poblacional/caracterizacion-poblacional.ts
+++ src/app/modules/servicios-salud/caracterizacion-poblacional/caracterizacion-poblacional.ts
@@ -1,12 +1,28 @@
+import { NgFor } from '@angular/common';
+import { File } from './../../../interfaces/acordion';
+import {cardButtons} from './../../../interfaces/salud';
 import { Component } from '@angular/core';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-caracterizacion-poblacional',
   standalone: true,
-  imports: [],
+  imports: [NgbAccordionModule, NgFor],
   templateUrl: './caracterizacion-poblacional.html',
   styleUrl: './caracterizacion-poblacional.css',
 })
 export class CaracterizacionPoblacional {
+  items: File[]=[];
+  cards : cardButtons[]=[];
+
+  constructor(){
+
+    this.cards =[
+      // Agrega tarjetas específicas para Caracterización Poblacional aquí
+    ];
+    this.items = [
+      // Agrega archivos específicos para Caracterización Poblacional aquí
+    ];
+  }
 
 }
